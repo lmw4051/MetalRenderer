5746d27e8e490adb44e3110172343ec590bb27d1
David <dvdlee41@gmail.com>
2020-10-29 15:31:22 +0800

    Initial Commit

---
+++ MetalRenderer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalRenderer
+//
+//  Created by David on 2020/10/29.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
