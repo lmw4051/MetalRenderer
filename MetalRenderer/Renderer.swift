ea4e21b71726d2c3a7a4b5cf30c999013097541f
David <dvdlee41@gmail.com>
2020-10-29 18:12:35 +0800

    1. Sending vertex data into metal buffers in swift file and pass them to the GPU for rendering 2. Set timer property for translating x animation

--- MetalRenderer/Renderer.swift
+++ MetalRenderer/Renderer.swift
@@ -15,6 +15,29 @@ class Renderer: NSObject {
   static var library: MTLLibrary!
   let pipelineState: MTLRenderPipelineState
   
+  let positionArray: [SIMD4<Float>] = [
+    SIMD4<Float>(-0.5, -0.2, 0, 1),
+    SIMD4<Float>(0.2, -0.2, 0, 1),
+    SIMD4<Float>(0, 0.5, 0, 1),
+    SIMD4<Float>(0, 0.5, 0, 1),
+    SIMD4<Float>(0.2, -0.2, 0, 1),
+    SIMD4<Float>(0.7, 0.7, 0, 1)
+  ]
+  
+  let colorArray: [SIMD3<Float>] = [
+    SIMD3<Float>(1, 0, 0),
+    SIMD3<Float>(0, 1, 0),
+    SIMD3<Float>(0, 0, 1),
+    SIMD3<Float>(0, 0, 1),
+    SIMD3<Float>(0, 1, 0),
+    SIMD3<Float>(1, 0, 1)
+  ]
+  
+  let positionBuffer: MTLBuffer
+  let colorBuffer: MTLBuffer
+  
+  var timer: Float = 0
+  
   init(view: MTKView) {
     guard let device = MTLCreateSystemDefaultDevice(),
       let commandQueue = device.makeCommandQueue() else {
@@ -24,6 +47,11 @@ class Renderer: NSObject {
     self.commandQueue = commandQueue
     Renderer.library = device.makeDefaultLibrary()
     pipelineState = Renderer.createPipelineState()
+    
+    let positionLength = MemoryLayout<SIMD4<Float>>.stride * positionArray.count
+    positionBuffer = device.makeBuffer(bytes: positionArray, length: positionLength, options: [])!
+    let colorLength = MemoryLayout<SIMD3<Float>>.stride * colorArray.count
+    colorBuffer = device.makeBuffer(bytes: colorArray, length: colorLength, options: [])!
     super.init()
   }
   
@@ -52,7 +80,16 @@ extension Renderer : MTKViewDelegate {
       let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor) else {
         return
     }
+    timer += 0.05
+    var currentTime = sin(timer)
+    commandEncoder.setVertexBytes(&currentTime,
+                                  length: MemoryLayout<Float>.stride,
+                                  index: 2)
+    
     commandEncoder.setRenderPipelineState(pipelineState)
+    commandEncoder.setVertexBuffer(positionBuffer, offset: 0, index: 0)
+    commandEncoder.setVertexBuffer(colorBuffer, offset: 0, index: 1)
+    
     commandEncoder.drawPrimitives(type: .triangle,
                                   vertexStart: 0,
                                   vertexCount: 6)
